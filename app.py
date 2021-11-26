336fa9eba3224c603cb43340399acf14927d9782
Vivek7 <viveklimbad1010@gmail.com>
2021-11-26 08:26:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, request
-nltk.download('all')
+nltk.download('brown')
+nltk.download('reuters')
+nltk.download('PlaintextCorpusReader')
 from nltk.downloader import download, download_shell 
 
 from pprint import pprint
